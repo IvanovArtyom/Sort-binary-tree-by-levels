c38d0b0cbc41ba7a8ed6c043efa7da2d569ed0f9
Артем Иванов <IvanovArtyom3000@yandex.ru>
2023-07-19 11:41:33 +0300

    Добавьте файлы проекта.

---
+++ Sort binary tree by levels/Node.cs
@@ -0,0 +1,16 @@
+﻿namespace Sort_binary_tree_by_levels
+{
+    public class Node
+    {
+        public Node Left;
+        public Node Right;
+        public int Value;
+
+        public Node(Node l, Node r, int v)
+        {
+            Left = l;
+            Right = r;
+            Value = v;
+        }
+    }
+}
---
+++ Sort binary tree by levels/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Sort_binary_tree_by_levels
+{
+    class Kata
+    {
+        public static void Main()
+        {
+            // Test
+            var t = TreeByLevels(new Node(new Node(null, new Node(null, null, 4), 2), new Node(new Node(null, null, 5), new Node(null, null, 6), 3), 1));
+            // ...should return {1,2,3,4,5,6}
+        }
+
+        public static List<int> TreeByLevels(Node node)
+        {
+            List<int> levels = new();
+
+            if (node == null)
+                return levels;
+
+            Queue<Node> queue = new();
+            queue.Enqueue(node);
+            
+            while (queue.Count != 0)
+            {
+                Node currentNode = queue.Dequeue();
+                levels.Add(currentNode.Value);
+
+                if (currentNode.Left != null)
+                    queue.Enqueue(currentNode.Left);
+
+                if (currentNode.Right != null)
+                    queue.Enqueue(currentNode.Right);
+            }
+
+            return levels;
+        }
+    }
+}
